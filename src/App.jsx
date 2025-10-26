77a2cfffca7f020c79235bcdd59a39c3c7ab8e4d
Aanchal Yadav <yadavaanchal2005@gmail.com>
2025-10-26 17:08:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,22 @@
-console.log('App placeholder');
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import EmotionDetector from "./components/EmotionDetector";
+import "./styles.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<EmotionDetector />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
