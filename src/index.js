4417a544fe136758a0718f73538284e1349962b6
Aanchal Yadav <yadavaanchal2005@gmail.com>
2025-10-26 19:05:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('React entry placeholder');
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // Tailwind + global styles
+import { BrowserRouter } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css'; // toastify styles
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
