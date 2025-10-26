d7ae2616be1bfca5d239203610ad713f37da3c1b
Aanchal Yadav <yadavaanchal2005@gmail.com>
2025-10-26 19:22:48 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // Tailwind + global styles
+// Tailwind + global styles
 import { BrowserRouter } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css'; // toastify styles
 
